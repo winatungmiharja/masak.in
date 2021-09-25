f8aad0eef4c1d8b410ce6cdcdd4df372742efd05
winatungmiharja <winatungmiharja@gmail.com>
2021-09-25 17:59:48 +0900

    feat: resizing session

--- src/components/Session/SessionVideo.js
+++ src/components/Session/SessionVideo.js
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Wrap } from "@chakra-ui/react";
+import { Maximize, Minimize, PlayCircle } from "react-feather";
 import ReactPlayer from "react-player";
 import "./Session.scss";
 
-export default function SessionVideo({ value }) {
+export default function SessionVideo({ value, toggleWindow, isVideoMode }) {
   const [isVideoReady, setIsVideoReady] = useState(false);
   return (
     <Box
-      bg="grey"
-      w="100%"
-      color="gray.500"
-      borderRadius={2}
-      className="overflow "
+      position="relative"
+      backgroundColor="white"
+      borderRadius="2xl"
+      overflow="hidden"
     >
-      <div className="session-video-player-wrapper">
-        <ReactPlayer
-          width="100%"
-          url={value}
-          controls
-          onStart={() => console.log("start video")}
-          onReady={() => setIsVideoReady(true)}
-        />
-
-        {!isVideoReady && <div className="video-loader"></div>}
-      </div>
+      <Box position="absolute" zIndex="1" right="10px" top="10px">
+        <Button
+          size="xs"
+          borderRadius="50%"
+          width="30px"
+          height="30px"
+          onClick={toggleWindow}
+          boxShadow="0px 8px 24px rgba(149, 157, 165, 0.2)"
+        >
+          {isVideoMode ? <Maximize /> : <Minimize />}
+        </Button>
+      </Box>
+      <Box>
+        <div className="video-wrapper">
+          <ReactPlayer
+            className="react-player"
+            controls={true}
+            playIcon={<PlayCircle color="#fff" width="30px" />}
+            playing
+            url={value}
+            light={true}
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </Box>
     </Box>
   );
 }
--- src/layout/Session.js
+++ src/layout/Session.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDisclosure } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useRecipe, findRecipeData } from "../data/Recipe";
 import Split from "react-split";
@@ -8,32 +9,78 @@ import SessionInfo from "../components/Session/SessionInfo";
 import SessionTodoList from "../components/Session/SessionTodoList";
 import SessionVideo from "../components/Session/SessionVideo";
 import SessionStep from "../components/Session/SessionStep";
+import SessionModal from "../components/Session/SessionModal";
 
 const Session = () => {
   let data = findRecipeData(useRecipe(), useParams().id);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isVideoMode, setIsVideoMode] = useState(false);
+  const [isStepMode, setIsStepMode] = useState(false);
+  const [allSize, setAllSize] = useState([50, 50]);
+  const [leftSize, setLeftSize] = useState([40, 60]);
+  const [rightSize, setRightSize] = useState([50, 50]);
+
+  const [isIngredientCompleted, setIsIngredientCompleted] = useState(false);
+  const videoModeWindow = () => {
+    if (isVideoMode) {
+      setAllSize([50, 50]);
+      setRightSize([50, 50]);
+    } else {
+      setAllSize([0, 100]);
+      setRightSize([100, 0]);
+    }
+    setIsVideoMode(!isVideoMode);
+  };
+  const stepModeWindow = () => {
+    if (isStepMode) {
+      setLeftSize([40, 60]);
+    } else {
+      setLeftSize([0, 100]);
+    }
+    setIsStepMode(!isStepMode);
+  };
+
   return (
     <Box>
-      <Split className="flex" sizes={[40, 60]} minSize={0} gutterSize={16}>
+      <SessionModal isOpen={isOpen} onClose={onClose} img={data.image} />
+      <Split className="flex" sizes={allSize} minSize={0} gutterSize={16}>
         <Split
+          sizes={leftSize}
           gutterSize={16}
           minSize={0}
           className="split"
           direction="vertical"
           style={{ height: "calc(100vh - 72px)" }}
         >
-          {data && <SessionInfo value={data} />}
-          <SessionTodoList value={data.ingredients} mealId={data.id} />
+          <SessionInfo value={data} />
+          <SessionStep
+            value={data.instructions}
+            mealId={data.id}
+            toggleWindow={() => stepModeWindow()}
+            isStepMode={isStepMode}
+            isIngredientCompleted={isIngredientCompleted}
+            onCompleted={onOpen}
+          />
         </Split>
         <Split
+          sizes={rightSize}
           gutterSize={16}
           minSize={0}
           className="split"
           direction="vertical"
           style={{ height: "calc(100vh - 72px)" }}
         >
-          <SessionVideo value={data.youtube} />
-
-          <SessionStep value={data.instructions} mealId={data.id} />
+          <SessionVideo
+            value={data.youtube}
+            toggleWindow={() => videoModeWindow()}
+            isVideoMode={isVideoMode}
+          />
+          <SessionTodoList
+            value={data.ingredients}
+            mealId={data.id}
+            isIngredientCompleted={isIngredientCompleted}
+            setIsIngredientCompleted={setIsIngredientCompleted}
+          />
         </Split>
       </Split>
     </Box>
